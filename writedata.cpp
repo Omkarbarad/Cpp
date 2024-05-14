ab156d92a5eb1356319de50eb72e4121fc28f715
Omkarbarad <164455064+Omkarbarad@users.noreply.github.com>
2024-05-14 11:29:59 +0530

    writedata.cpp

--- writedata.cpp
+++ writedata.cpp
@@ -3,21 +3,16 @@
 using namespace std;
 
 int main() {
-    // Open a file named "data.txt" for writing
+
     ofstream file("data.txt");
 
-    // Check if the file is opened successfully
     if (!file.is_open()) {
         cout << "Failed to open file!" << endl;
         return 1;
     }
-
-    // Write data to the file
     file << "This is a sample text.\n";
     file << "Writing data to a file.\n";
     file << "End of file.";
-
-    // Close the file
     file.close();
 
     cout << "Data has been written to the file successfully." << endl;
